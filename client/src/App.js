d75d04de63884615a769426a585199eee3b8ee43
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-05-30 21:44:00 +0300

    Adding Bar Chart Design

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import LineChartDemo from "./components/LineChart";
 import PieChartDesign from "./components/PieChart";
+import BarChartDesign from "./components/BarChartDesign";
 
 const App = () => {
   return (
     <React.Fragment>
       <LineChartDemo />
       <PieChartDesign />
+      <BarChartDesign />
     </React.Fragment>
   );
 };
---
+++ client/src/components/BarChartDesign.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BarChartDesign = () => {
+  return (
+    <React.Fragment>
+        <div>
+            <h1>Bar Chart Design</h1>
+        </div>
+    </React.Fragment>
+  )
+}
+
+export default BarChartDesign
