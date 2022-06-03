4c3e2a9bea47c91e72f4aabbd083e3980258565a
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-06-03 23:55:27 +0300

    Adding Area Chart

--- client/src/components/AreaChart.js
+++ client/src/components/AreaChart.js
@@ -1,9 +1,7 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 
 const AreaChart = () => {
-
-  const [list,setList] = useState([]);
-
+  const [list, setList] = useState([]);
 
   const data = [
     {
@@ -50,17 +48,47 @@ const AreaChart = () => {
     },
   ];
 
-
-
-
   return (
     <React.Fragment>
-      <h2>Are Chart</h2>
-      {[1,2,3,4,5].map((c,index)=>(
-        <>
-        <p>{c}</p>
-        </>
-      ))}
+      <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="uv"
+            stackId="1"
+            stroke="#8884d8"
+            fill="#8884d8"
+          />
+          <Area
+            type="monotone"
+            dataKey="pv"
+            stackId="1"
+            stroke="#82ca9d"
+            fill="#82ca9d"
+          />
+          <Area
+            type="monotone"
+            dataKey="amt"
+            stackId="1"
+            stroke="#ffc658"
+            fill="#ffc658"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
     </React.Fragment>
   );
 };
